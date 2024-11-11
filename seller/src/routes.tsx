2dbf7834adb3b4aa631a55d79dc681edff45c619
Jonatahn Bajada <jbajada@ryerson.ca>
2024-11-11 20:09:37 +0700

    installed rainbowkit

--- seller/src/routes.tsx
+++ seller/src/routes.tsx
@@ -1,28 +1,42 @@
 import OrdersComponent from './pages/orders/OrdersComponent.tsx';
+import Login from './pages/login/Login.tsx';
 import NotFoundComponent from './pages/NotFoundComponent.tsx';
-import { createRootRoute, createRoute, createRouter } from '@tanstack/react-router';
+import {
+    createRootRoute,
+    createRoute,
+    createRouter,
+} from '@tanstack/react-router';
 import RootComponent from './pages/RootComponent.tsx';
-
-const routes= [{
-    path: '/',
-    component: OrdersComponent,
-},{
-    path: '*',
-    component: NotFoundComponent,
-}]
 
 // Create the root route
 const rootRoute = createRootRoute({
     component: RootComponent, // Optional root component
 });
 
+const routes = [
+    {
+        path: '/',
+        component: OrdersComponent,
+    },
+    {
+        path: '/login',
+        component: Login,
+    },
+    {
+        path: '*',
+        component: NotFoundComponent,
+    },
+];
+
 // Add child routes to the root route
-rootRoute.addChildren(routes.map(route => {
-    return createRoute({
-        ...route,
-        getParentRoute: () => rootRoute, // Specify the parent route
+rootRoute.addChildren(
+    routes.map((route) => {
+        return createRoute({
+            ...route,
+            getParentRoute: () => rootRoute, // Specify the parent route
+        });
     })
-}));
+);
 
 // Create the router
 export const router = createRouter({
