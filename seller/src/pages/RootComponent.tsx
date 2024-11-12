8dc44c1cbb96a11bb5ea7f3c0b7bba5002db8d42
Jonatahn Bajada <jbajada@ryerson.ca>
2024-11-12 22:24:22 +0700

    updated top dashboard

--- seller/src/pages/RootComponent.tsx
+++ seller/src/pages/RootComponent.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Outlet } from '@tanstack/react-router';
+import { SidebarTrigger } from '@/components/ui/sidebar';
 
 const RootComponent = () => {
     return (
         <div>
-            <h1 className='ml-4 hover:text-red-500 text-blue-500'>Hamza seller</h1>
+            <div className="flex items-center bg-black h-[56px]">
+                <SidebarTrigger className="ml-2" />
+
+                <div className="flex  ml-auto">
+                    <div className="h-[56px] border-l-2 border-white mx-4" />
+
+                    <button className="flex justify-center items-center h-[56px] w-[190px]">
+                        <div className="flex flex-col items-center">
+                            <p className="text-lg font-semibold">John Doe</p>
+                            <p className="text-sm">Admin</p>
+                        </div>
+                    </button>
+                </div>
+            </div>
             <Outlet />
         </div>
     );
