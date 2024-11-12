81459c72caaa7a126ba1f1c959a0353cadc9e1a9
Jonatahn Bajada <jbajada@ryerson.ca>
2024-11-12 22:38:35 +0700

    updated top dash

--- seller/src/pages/RootComponent.tsx
+++ seller/src/pages/RootComponent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from '@tanstack/react-router';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { CgBell } from 'react-icons/cg';
+import { FaSortDown } from 'react-icons/fa';
 
 const RootComponent = () => {
     return (
@@ -16,11 +17,15 @@ const RootComponent = () => {
 
                     <div className="h-[56px] border-l-[1px] border-white mx-4" />
                     {/* Login */}
-                    <button className="flex justify-center items-center bg-[#121212] h-[56px] w-[190px]">
-                        <div className="flex flex-col items-center">
-                            <p className="text-lg font-semibold">John Doe</p>
-                            <p className="text-sm">Admin</p>
+                    <button className="flex justify-evenly items-center bg-[#121212] h-[56px] w-[200px]">
+                        <div className="h-[40px] w-[40px] rounded-full bg-white"></div>
+                        <div className="flex flex-col w-[89px] mx-2">
+                            <p className="text-lg font-semibold leading-none mr-auto">
+                                John Doe
+                            </p>
+                            <p className="text-sm mr-auto">Admin</p>
                         </div>
+                        <FaSortDown />
                     </button>
                 </div>
             </div>
