d8e0155b67e0d2f31b356f7cc1d0e564920a0873
Tronky <john.kosinski@gmail.com>
2024-11-15 17:07:05 +0700

    added signature verification to auth route

--- hamza-server/src/api/seller/auth/route.ts
+++ hamza-server/src/api/seller/auth/route.ts
@@ -1,6 +1,15 @@
 import type { MedusaRequest, MedusaResponse, Logger } from '@medusajs/medusa';
 import { RouteHandler } from '../../route-handler';
 import jwt from 'jsonwebtoken';
+import UserRepository from '../../../repositories/user';
+import StoreRepository from '../../../repositories/store';
+import { SiweMessage } from 'siwe';
+import { Store } from '../../../models/store';
+import { FindOneOptions as TypeOrmFindOneOptions } from 'typeorm';
+
+interface FindOneOptions<Store> extends TypeOrmFindOneOptions<Store> {
+    owner_id?: string;
+}
 
 /**
  * Authenticates a store by wallet login.
@@ -17,6 +26,43 @@ export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
     ]);
 
     await handler.handle(async () => {
+        const { message, signature } = handler.inputParams;
+        const wallet_address = message.address?.trim()?.toLowerCase();
+
+        //verify the signature
+        const siweMessage = new SiweMessage(message);
+        let siweResponse = await siweMessage.verify({ signature });
+        handler.logger.debug(
+            'siwe response is ' + JSON.stringify(siweResponse)
+        );
+        if (!siweResponse.success) {
+            throw new Error('Error in validating wallet address signature');
+        }
+
+        //get the user record
+        let storeOwner = await UserRepository.findOne({
+            where: { wallet_address },
+        });
+
+        if (!storeOwner) {
+            return handler.returnStatusWithMessage(
+                404,
+                `User with wallet address ${wallet_address} not found.`
+            );
+        }
+
+        //get the corresponding store
+        const options: FindOneOptions<Store> = { owner_id: storeOwner.id };
+        const store = await StoreRepository.findOne(options);
+
+        if (!store) {
+            return handler.returnStatusWithMessage(
+                404,
+                `Store with owner ${storeOwner.id} not found.`
+            );
+        }
+
+        //once authorized, return a JWT token
         const token = jwt.sign(
             {
                 store_id: handler.inputParams.store_id,
