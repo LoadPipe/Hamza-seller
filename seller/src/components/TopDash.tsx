8026084c24fcd16e3722014c4cf1ef2f925f6036
Jonatahn Bajada <jbajada@ryerson.ca>
2024-11-19 15:39:29 +0700

    added back sidebar trigger

--- seller/src/components/TopDash.tsx
+++ seller/src/components/TopDash.tsx
@@ -9,7 +9,7 @@ const TopDash = () => {
             {/* Top Navigation */}
             <nav className="w-full py-4">
                 <div className="max-w-[1280px] mx-auto flex items-center justify-between px-4">
-                    <img src={HamzaLogo} />
+                    <SidebarTrigger className="ml-2" />
 
                     <div className="flex self-center">
                         <div className="flex justify-center items-center h-[50px] w-[50px] rounded-full bg-[#121212]">
@@ -27,38 +27,3 @@ const TopDash = () => {
 };
 
 export default TopDash;
-
-/*
-const TopDash = () => {
-    return (
-        <nav className="w-full py-4">
-            <div className="max-w-[1280px] mx-auto flex items-center justify-between px-4">
-                <img src={HamzaLogo} />
-                <WalletConnect />
-
-                <SidebarTrigger className="ml-2" />
-                <div className="flex  ml-auto">
-                    <div className="flex justify-center items-center h-[56px] w-[56px] rounded-full bg-[#121212]">
-                        <BellRing size={'24px'} />
-                    </div>
-
-                    <div className="h-[56px] border-l-[1px] border-white mx-4" />
-
-                    <div className="self-center">
-                        <WalletConnect />
-                    </div>
-                     <button className="flex justify-evenly items-center bg-[#121212] h-[56px] w-[200px]">
-                    <div className="h-[40px] w-[40px] rounded-full bg-white"></div>
-                    <div className="flex flex-col w-[89px] mx-2">
-                        <p className="text-lg font-semibold leading-none mr-auto">
-                            John Doe
-                        </p>
-                        <p className="text-sm mr-auto">Admin</p>
-                    </div>
-                    <ChevronDown />
-                </button>
-                </div>
-            </div>
-        </nav>
-    );
-};*/
