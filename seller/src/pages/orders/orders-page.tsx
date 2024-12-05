8664650122abff4872d0a8862abc61b58d9f0802
Jonatahn Bajada <jbajada@ryerson.ca>
2024-12-05 13:18:00 +0700

    brought back the jtw field

--- seller/src/pages/orders/orders-page.tsx
+++ seller/src/pages/orders/orders-page.tsx
@@ -31,7 +31,7 @@ async function getSellerOrders(
             : { field: 'created_at', direction: 'ASC' };
 
         const response = await postSecure('/seller/order', {
-            store_id: 'store_01JE6DQAQ1DK152V4ZZDNPH9YV',
+            store_id: getJwtField('store_id'),
             page: pageIndex,
             count: pageSize,
             filter: filters, // Add filters here
