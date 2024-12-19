37493b7091ab2200aa02cd548377350f3ae35a9e
Jonatahn Bajada <jbajada@ryerson.ca>
2024-12-19 18:20:06 +0700

    forces correct chain

--- admin-client/src/rainbowkit/wagmi.ts
+++ admin-client/src/rainbowkit/wagmi.ts
@@ -1,16 +1,12 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import {
-    arbitrum,
-    base,
-    mainnet,
-    optimism,
-    polygon,
-    sepolia,
-} from 'wagmi/chains';
+import { optimism, sepolia } from 'wagmi/chains';
+
+const allowedChains =
+    process.env.NEXT_PUBLIC_INCLUDE_SEPOLIA === 'true' ? sepolia : optimism;
 
 export const config = getDefaultConfig({
     appName: 'My RainbowKit App',
     projectId: 'YOUR_PROJECT_ID',
-    chains: [optimism, sepolia],
+    chains: [allowedChains],
     ssr: true, // If your dApp uses server side rendering (SSR)
 });
