fef94c4da2268aafa834e32b6a45494bb3fdd6fd
Garo <garonazarian09@gmail.com>
2025-01-06 20:46:18 +0700

    fixed action button styling

--- admin-client/src/pages/products/product-columns.tsx
+++ admin-client/src/pages/products/product-columns.tsx
@@ -158,44 +158,46 @@ export const generateColumns = (
                         const product = row.original;
 
                         return (
-                            <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                    <Button
-                                        variant="ghost"
-                                        className="h-8 w-8 p-0"
-                                    >
-                                        <span className="sr-only">
-                                            Open menu
-                                        </span>
-                                        <MoreHorizontal className="h-4 w-4" />
-                                    </Button>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent align="end">
-                                    <DropdownMenuLabel>
-                                        Actions
-                                    </DropdownMenuLabel>
-                                    <DropdownMenuItem
-                                        onClick={() =>
-                                            navigator.clipboard.writeText(
-                                                product.id
-                                            )
-                                        }
-                                    >
-                                        Copy Product ID
-                                    </DropdownMenuItem>
-                                    <DropdownMenuSeparator />
-                                    <DropdownMenuItem
-                                        onClick={() => console.log(product)}
-                                    >
-                                        Edit Product
-                                    </DropdownMenuItem>
-                                    <DropdownMenuItem
-                                        onClick={() => console.log(product)}
-                                    >
-                                        Delete Product
-                                    </DropdownMenuItem>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
+                            <div className="flex justify-end">
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <Button
+                                            variant="ghost"
+                                            className="h-8 w-8 p-0"
+                                        >
+                                            <span className="sr-only">
+                                                Open menu
+                                            </span>
+                                            <MoreHorizontal className="h-4 w-4" />
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent align="end">
+                                        <DropdownMenuLabel>
+                                            Actions
+                                        </DropdownMenuLabel>
+                                        <DropdownMenuItem
+                                            onClick={() =>
+                                                navigator.clipboard.writeText(
+                                                    product.id
+                                                )
+                                            }
+                                        >
+                                            Copy Product ID
+                                        </DropdownMenuItem>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem
+                                            onClick={() => console.log(product)}
+                                        >
+                                            Edit Product
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem
+                                            onClick={() => console.log(product)}
+                                        >
+                                            Delete Product
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </div>
                         );
                     },
                 };
