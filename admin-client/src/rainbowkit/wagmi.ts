1aaefdb683d02fe2a3bdcf7cb6d95df1de94fd2f
Tronky <john.kosinski@gmail.com>
2025-01-08 23:22:34 +0700

    cleaned up chain config

--- admin-client/src/rainbowkit/wagmi.ts
+++ admin-client/src/rainbowkit/wagmi.ts
@@ -1,15 +1,12 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { sepolia, polygonAmoy } from 'wagmi/chains';
-
-const allowedChains =
-    import.meta.env.VITE_INCLUDE_SEPOLIA === 'true' ? sepolia : optimism;
-
-console.log(import.meta.env.VITE_INCLUDE_SEPOLIA);
-console.log('ALLOWED CHAINS:', allowedChains);
+import { sepolia, optimism, polygonAmoy } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
     appName: 'My RainbowKit App', //TODO: change these defaults
     projectId: 'YOUR_PROJECT_ID',
-    chains: [sepolia, polygonAmoy],
+    chains:
+        import.meta.env.VITE_INCLUDE_SEPOLIA === 'true'
+            ? [sepolia, polygonAmoy]
+            : [optimism],
     ssr: true, // If your dApp uses server side rendering (SSR)
 });
