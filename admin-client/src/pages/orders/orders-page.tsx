fa151852e0f3f934d08ec09cd7a1ec679bc93d79
Garo <garonazarian09@gmail.com>
2025-01-13 15:02:08 +0700

    - Api refactor; cleanup to api page.

--- admin-client/src/pages/orders/orders-page.tsx
+++ admin-client/src/pages/orders/orders-page.tsx
@@ -5,59 +5,15 @@ import { z } from 'zod';
 import React from 'react';
 import { useSearch } from '@tanstack/react-router';
 import { OrderSearchSchema } from '@/routes.tsx';
-import { getJwtStoreId } from '@/utils/authentication';
-import { postSecure } from '@/utils/api-calls';
 import { filterStore } from '@/stores/order-filter/order-filter-store.ts';
 import { useStore } from '@tanstack/react-store';
 import { SortingState } from '@tanstack/react-table';
-import {
-    saveStatusCountToStorage,
-    updateStatusCount,
-} from '@/stores/order-filter/order-filter-store';
 import { useNavigate } from '@tanstack/react-router';
 import { setFilter } from '@/stores/order-filter/order-filter-store.ts';
 import { ReleaseEscrow } from '@/components/orders/release-escrow.tsx';
+import { getSellerOrders } from '@/pages/orders/api/seller-orders.ts';
 
 type Order = z.infer<typeof OrderSchema>;
-
-async function getSellerOrders(
-    pageIndex = 0,
-    pageSize = 10,
-    filters: Record<string, any>,
-    sorting: SortingState = []
-): Promise<{ orders: Order[]; totalRecords: number }> {
-    try {
-        const sort = sorting[0]
-            ? {
-                  field: sorting[0].id,
-                  direction: sorting[0].desc ? 'DESC' : 'ASC',
-              }
-            : { field: 'created_at', direction: 'DESC' };
-
-        const response = await postSecure('/seller/order', {
-            store_id: getJwtStoreId(),
-            page: pageIndex,
-            count: pageSize,
-            filter: filters, // Add filters here
-            sort: sort,
-        });
-
-        // SS orders: object => typecast: object ...
-        const data: object = response.orders as object;
-        // SS totalRecords: string => typecast: number...
-        const totalRecords: number = response.totalRecords as number;
-        // console.log(`TOTAL RECORDS: ${JSON.stringify(response.statusCount)}`);
-        saveStatusCountToStorage(response.statusCount);
-        updateStatusCount(response.statusCount);
-        return {
-            orders: OrderSchema.array().parse(data), // Validate using Zod
-            totalRecords,
-        };
-    } catch (error) {
-        console.error('Failed to fetch seller orders:', error);
-        throw new Error('Failed to fetch seller orders');
-    }
-}
 
 export default function OrdersPage() {
     const { filters } = useStore(filterStore); // Subscribe to filter store
