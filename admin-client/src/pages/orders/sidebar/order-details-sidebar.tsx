469cc7e7109de013b466cc6e4282010aa53b1cfa
Jonatahn Bajada <jbajada@ryerson.ca>
2025-01-30 15:06:18 +0700

    fixerd currency total issue

--- admin-client/src/pages/orders/sidebar/order-details-sidebar.tsx
+++ admin-client/src/pages/orders/sidebar/order-details-sidebar.tsx
@@ -185,6 +185,12 @@ export function OrderDetailsSidebar() {
         histories: orderDetails.histories,
         refunds: orderDetails.refunds,
     };
+
+    const shippingFee = formatCryptoPrice(
+        orderDetails?.shipping_methods[0]?.price ?? 0,
+        currencyCode
+    );
+
     const totalPrice = (orderDetails?.items || []).reduce(
         (acc: number, item: any) => {
             const unitPrice = Number(item.unit_price) || 0; // Ensure it's a number
@@ -193,6 +199,14 @@ export function OrderDetailsSidebar() {
         },
         0 // Initial accumulator value
     );
+
+    //Grand total calculated
+    const totalPriceFormatted = formatCryptoPrice(
+        totalPrice ?? 0,
+        currencyCode
+    );
+    const grandTotal = Number(totalPriceFormatted) + Number(shippingFee);
+
     return (
         <div>
             <Sidebar
@@ -439,10 +453,7 @@ export function OrderDetailsSidebar() {
                                     currencyCode
                                 )} // Adjust as needed
                                 currencyCode={currencyCode}
-                                total={formatCryptoPrice(
-                                    orderDetails?.payments[0]?.amount ?? 0,
-                                    currencyCode
-                                )}
+                                total={grandTotal ?? 0}
                             />
 
                             <hr className="border-primary-black-65 w-full mx-auto my-[32px]" />
