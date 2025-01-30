716b88a309c866355184677d11f59d4a32570c1c
Garo <garonazarian09@gmail.com>
2025-01-30 12:33:23 +0700

    sorting fix && refactor naming and structure

--- admin-client/src/pages/products/product-category-page.tsx
+++ admin-client/src/pages/products/product-category-page.tsx
@@ -46,7 +46,7 @@ export default function ProductCategoryPage() {
         },
     });
 
-    // Define table productColumns
+    // Define table orderColumns
     const columns: ColumnDef<Category>[] = [
         {
             accessorKey: 'id',
