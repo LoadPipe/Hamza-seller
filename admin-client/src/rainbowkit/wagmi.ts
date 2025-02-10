9b507429a5fb04a1d22b34679ee5eb5c20383da8
Jonathan Bajada <33204110+JonathanBajada@users.noreply.github.com>
2025-02-10 20:34:05 +0700

    HAMSL-328: multi chain (#178)
    * can now change chain by env variable
    
    * updated env example
    
    * cleaned up file
    
    * fixed build error

--- admin-client/src/rainbowkit/wagmi.ts
+++ admin-client/src/rainbowkit/wagmi.ts
@@ -6,15 +6,53 @@ import {
     base,
     polygon,
     arbitrum,
-    bsc,
+    mainnet,
 } from 'wagmi/chains';
+import { _chains } from 'node_modules/@rainbow-me/rainbowkit/dist/config/getDefaultConfig';
+import { RainbowKitChain } from 'node_modules/@rainbow-me/rainbowkit/dist/components/RainbowKitProvider/RainbowKitChainContext';
+
+let wagmiChains: RainbowKitChain[] = [];
+
+let allowedChains = (import.meta.env.VITE_ALLOWED_BLOCKCHAINS ?? '').split(',');
+
+if (allowedChains.length === 0 || allowedChains[0] === '') {
+    allowedChains = ['sepolia'];
+}
+
+const chainConfig: Record<string | number, RainbowKitChain> = {
+    optimism: optimism,
+    10: optimism,
+    polygon: polygon,
+    137: polygon,
+    mainnet: mainnet,
+    1: mainnet,
+    sepolia: sepolia,
+    11155111: sepolia,
+    polygonAmoy: polygonAmoy,
+    80002: polygonAmoy,
+    base: base,
+    8453: base,
+    arbitrum: arbitrum,
+    42161: arbitrum,
+};
+
+// Populate wagmiChains based on allowedChains
+wagmiChains = allowedChains
+    .map((c: any) => chainConfig[c as keyof typeof chainConfig])
+    .filter(Boolean) as RainbowKitChain[]; // Remove undefined values
+
+// Ensure wagmiChains has at least one item before assigning it to wagmiChains2
+if (wagmiChains.length === 0) {
+    throw new Error(
+        'wagmiChains is empty; at least one chain is required for _chains.'
+    );
+}
+
+let allowedWagmiChains: _chains = [wagmiChains[0], ...wagmiChains.slice(1)];
 
 export const config = getDefaultConfig({
-    appName: 'My RainbowKit App', //TODO: change these defaults
+    appName: 'My RainbowKit App',
     projectId: 'YOUR_PROJECT_ID',
-    chains:
-        import.meta.env.VITE_INCLUDE_SEPOLIA === 'true'
-            ? [sepolia, polygonAmoy]
-            : [sepolia, optimism, base, polygon, arbitrum, bsc],
-    ssr: true, // If your dApp uses server side rendering (SSR)
+    chains: allowedWagmiChains,
+    ssr: true,
 });
