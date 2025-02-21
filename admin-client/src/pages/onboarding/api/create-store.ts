02b213fda064e3cd004259704d1f57585f36bc35
Tronky <john.kosinski@gmail.com>
2025-02-21 15:44:54 +0700

    updated api routes for Hamza-backend

--- admin-client/src/pages/onboarding/api/create-store.ts
+++ admin-client/src/pages/onboarding/api/create-store.ts
@@ -1,8 +1,8 @@
 import { postSecure } from '@/utils/api-calls';
-import {getJwtWalletAddress } from '@/utils/authentication';
+import { getJwtWalletAddress } from '@/utils/authentication';
 
 export const updateOnboardingByWalletId = async (data: any) => {
-    const response = await postSecure(`/seller/onboarding/create-store`, {
+    const response = await postSecure(`/seller/store`, {
         wallet_address: getJwtWalletAddress(),
         ...data,
     });
